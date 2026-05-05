17f4b3f0a000973692acc9c0c4fc4732c69ce426
Jose Horacio <jhoracio19@hotmail.com>
2026-05-04 20:19:24 -0600

    agregando proteccion a las rutas

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
+  // Aquí agregamos nuestra regla de redirecciones mágicas
+  async redirects() {
+    return [
+      {
+        source: '/todo-incluido',
+        destination: '/banquetes',
+        permanent: true, // Esto le dice a Google que la mudanza es definitiva (SEO de oro)
+      },
+      {
+        source: '/cotizador',
+        destination: '/renta-salon',
+        permanent: true,
+      },
+    ];
   },
-  images: {
-    unoptimized: true,
-  },
-}
+};
 
-export default nextConfig
+module.exports = nextConfig; // o export default nextConfig si usas .mjs
